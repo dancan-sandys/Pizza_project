105f0e9aa5e503bc304bab5e106fc4ec9a117129
dancan-sandys <dancan.oruko96@gmail.com>
2020-08-03 08:57:28 +0300

    indent all the files

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 //business logic
 
 function Pizza(size, crust, topping, delivery) {
+
     this.size = size;
     this.crust = crust;
     this.topping = topping;
     this.delivery = delivery;
 }
 
+
 Pizza.prototype.calculatingSizePrice = function () {
+
     if (this.size == `large`) {
         sizePrice = 500;
         return sizePrice
@@ -28,6 +31,7 @@ Pizza.prototype.calculatingSizePrice = function () {
 
 
 Pizza.prototype.calculatingCrustRatio = function () {
+
     if (this.crust == `Sicilian`) {
         crustRatio = 0.3;
         return crustRatio
@@ -46,6 +50,7 @@ Pizza.prototype.calculatingCrustRatio = function () {
 }
 
 Pizza.prototype.calculatingToppingRatio = function () {
+
     if (this.topping == `Pepperoni`) {
         toppingRatio = 0.2;
         return toppingRatio
@@ -64,6 +69,7 @@ Pizza.prototype.calculatingToppingRatio = function () {
 }
 
 Pizza.prototype.deliver = function () {
+
     if (this.delivery == `Yes`) {
         $(`.deliveryform`).toggle();
         return 70
@@ -73,10 +79,12 @@ Pizza.prototype.deliver = function () {
 }
 
 Pizza.prototype.price = function () {
+
     thePrice = this.calculatingSizePrice() +
         (this.calculatingToppingRatio() * this.calculatingSizePrice()) +
         (this.calculatingCrustRatio() * this.calculatingSizePrice()) +
-        this.deliver();
+        this.deliver()
+    ;
 
     return thePrice
 }
@@ -100,10 +108,12 @@ $(`form`).submit(function (event) {
 
     newPizza = new Pizza(pizzaSize, pizzaCrust, pizzaTopping, pizzaDelivery)
 
-    $(`#calculator`).text(`${newPizza.size} + ${newPizza.topping} + ${newPizza.crust} = ksh ${newPizza.price()}`
+    $(`#calculator`).text(`${newPizza.size} + ${newPizza.topping} + ${newPizza.crust} = 
+      ksh ${newPizza.price()}`
     )
 
-    $(`#orders`).append(`<li> ${newPizza.size} + ${newPizza.topping} + ${newPizza.crust} = ksh ${newPizza.price()} </li>`
+    $(`#orders`).append(`<li> ${newPizza.size} + ${newPizza.topping} + ${newPizza.crust} = 
+      ksh ${newPizza.price()} </li>`
     )
 
     totals += newPizza.price()
@@ -114,36 +124,50 @@ $(`form`).submit(function (event) {
 
 
 $(`#submit`).click(function () {
+    
     var address = $(`#address`).val();
     var City = $(`#City`).val();
     var buiding = $(`#Building`).val();
 
     $(`.deliveryform`).toggle();
+    
     alert(`Your order will be delivered to ${address}, ${City}, ${buiding}.
     The delivery cost is ksh 70 and will be added to your first order only`)
+    
     $(`#deliver`).val(`No`);
+    
     $(`.deliver`).toggle();
+
 })
 
 
 $(`.hamburger`).click(function () {
+    
     $(`.nav`).addClass(`open`);
 
     $(`.hamburger-open`).toggle();
+    
     $(`.orders`).toggle();
+    
     $(`.buy`).toggle();
 
 })
 
 $(`.hamburger-open`).click(function () {
+    
     $(`.nav`).removeClass(`open`);
+    
     $(`.orders`).toggle();
+    
     $(`.buy`).toggle();
+    
     $(`.hamburger-open`).toggle();
+
 })
 
 
 $(`.buy`).click(function () {
+
     alert(`Welcome to the Pizza experience! Your order is being processed`)
 
 })
